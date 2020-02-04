412f65549d35fc4d458931746d2c736739ef82f4
Ihor Sikaliuk <aw9810@hotmail.com>
2020-02-04 20:57:17 +0200

    upload

--- Directory viewer/Program.cs
+++ Directory viewer/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Directory_viewer
 {
     class Program
     {
+        static void checkDirectory(DirectoryInfo directory, int counter)
+        {
+            for (int i = 0; i < counter; i++)
+                Console.Write(" - ");
+            Console.WriteLine(directory);
+            DirectoryInfo[] list = directory.GetDirectories();
+            foreach (DirectoryInfo dir in list)
+                checkDirectory(dir, counter + 1);
+        }
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            DirectoryInfo startDirectory = new DirectoryInfo(Environment.CurrentDirectory);
+            checkDirectory(startDirectory, 0);
+            Console.ReadKey();
         }
     }
 }
